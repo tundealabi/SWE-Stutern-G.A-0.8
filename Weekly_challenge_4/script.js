0c25a2e77a2599b36e6c0894b9ca9e6fe622029f
tundealabi <61058516+tundealabi@users.noreply.github.com>
2020-07-28 09:46:51 +0100

    Add extra constraints

--- Weekly_challenge_4/script.js
+++ Weekly_challenge_4/script.js
@@ -2,9 +2,13 @@
 function minCost(cost){
   let max = 100;
   let arr = [];
+  if(cost.length > 100 || cost.length < 1)
+       return "This neighbourhood can only take a max of 100 houses and a min of one house.";
     for(let i = 0; i < cost.length; i+=1){
-     for(let j =0; j < cost[i].length; j+=1){
-       if(cost[i][j] < max && arr[i-1] !== cost[i][j]){
+      for(let j =0; j < cost[i].length; j+=1){
+        if(cost[i][j] < 1 || cost[i][j] > 99)
+              return `Cost for each material can only be within the range of $1 - $100.`;
+        if(cost[i][j] < max && arr[i-1] !== cost[i][j]){
            arr[i] = cost[i][j];
             max = cost[i][j];                    
        }
@@ -12,8 +16,7 @@ function minCost(cost){
         max = 100;
     }
     const reduced = arr.reduce((total,number) => total + number);
- return `The minimum cost to build all the houses in the
-neighborhood based on the array passed is equal to $${reduced}.`;
+ return `The minimum cost to build all the houses in the neighborhood based on the array passed is equal to $${reduced}.`;
 }
 
 minCost([[2, 2, 3], [1, 2, 3], [3, 3, 1]]);
