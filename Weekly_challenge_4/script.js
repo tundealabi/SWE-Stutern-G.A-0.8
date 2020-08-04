31c87b6118401907c6d3ce19fdfcae14dc41790a
tundealabi <61058516+tundealabi@users.noreply.github.com>
2020-08-04 10:02:07 +0100

    Implement a more direct approach

--- Weekly_challenge_4/script.js
+++ Weekly_challenge_4/script.js
@@ -2,27 +2,19 @@
 function minCost(cost){
   let max = 100;
   let arr = [];
+  let arrIndex = [];
   if(cost.length > 100 || cost.length < 1)
        return "This neighbourhood can only take a max of 100 houses and a min of two houses.";
     for(let i = 0; i < cost.length; i+=1){
       for(let j =0; j < cost[i].length; j+=1){
         if(cost[i][j] < 1 || cost[i][j] > 99)
               return `Cost for each material can only be within the range of $1 - $100.`;
-      if(i == 0){
-              if(cost[i][j] < max){
-             arr[i] = cost[i][j]
+      
+         if(cost[i][j] < max && j !== arrIndex[i-1]){
+             arr[i] = cost[i][j];
              max = cost[i][j];
-           
-          }
-        }else if(i>0){
-          if(cost[i][j] <= max && cost[i-1].lastIndexOf(arr[i-1]) != cost[i].lastIndexOf(cost[i][j])){
-             arr[i] = cost[i][j]
-             max = cost[i][j];
-             console.log(arr)
-                   
-          }
-        }
-               
+             arrIndex[i] = j;
+         }
      }
        max = 100;
     };
@@ -30,4 +22,4 @@ function minCost(cost){
  return `The minimum cost to build all the houses in the neighborhood based on the array passed is equal to $${reduced}.`;
 }
 
-minCost([[2, 2, 3], [1, 2, 3], [3, 3, 1]]);
+minCost([[2, 2, 3], [1, 2, 3], [3, 3, 1]]); //$5
